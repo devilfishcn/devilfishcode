d4db5b4e1dc35ebf10dcfde92b804227c4658624
devilfishcn <devilfishcn@126.com>
2017-09-21 18:42:26 +0800

    增加广播内容

---
+++ rabbit/publisher.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import pika
+import time
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',5672))
+channel = connection.channel()
+channel.exchange_declare(exchange='logs',type='fanout')
+
+i = 0
+while(i<10):
+    i+=1
+    msg='hello world '+str(i)
+    channel.basic_publish(exchange='logs',routing_key='', body=msg)
+    time.sleep(2)
+    print "send msg: %s" % msg
+connection.close()
---
+++ rabbit/subscriber.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import pika
+import time
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.exchange_declare(exchange='logs',type='fanout')
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
+print("random queuename:", queue_name)
+channel.queue_bind(exchange='logs',queue=queue_name)
+def callback(ch, method, properties, body):
+    print(" [x] %r" % body)
+    time.sleep(3)
+channel.basic_consume(callback,queue=queue_name,no_ack=True)
+channel.start_consuming()
