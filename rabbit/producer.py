055d1b26191c7d0bb63cd8f59a53c769520dcdf4
devilfishcn <devilfishcn@126.com>
2017-09-21 16:46:38 +0800

    增加消息持久化

--- rabbit/producer.py
+++ rabbit/producer.py
@@ -13,6 +13,9 @@ channel.queue_declare(queue='hello2',durable=True)
 # RabbitMQ a message can never be sent directly to the queue, it always needs to go through an exchange.
 channel.basic_publish(exchange='',
                       routing_key='hello2',  # queue名字
-                      body='Hello World!')  # 消息内容
+                      body='Hello World!',
+                      properties=pika.BasicProperties(
+                          delivery_mode=2,  # make message persistent
+                          ))  # 消息内容
 print(" [x] Sent 'Hello World!'")
 connection.close()  # 队列关闭
