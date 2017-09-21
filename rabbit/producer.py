f35ffe87b3824551859acf20de729b82f15495fb
devilfishcn <devilfishcn@126.com>
2017-09-21 16:38:58 +0800

    增加序列化

--- rabbit/producer.py
+++ rabbit/producer.py
@@ -12,7 +12,7 @@ channel = connection.channel()
 channel.queue_declare(queue='hello2',durable=True)
 # RabbitMQ a message can never be sent directly to the queue, it always needs to go through an exchange.
 channel.basic_publish(exchange='',
-                      routing_key='hello',  # queue名字
+                      routing_key='hello2',  # queue名字
                       body='Hello World!')  # 消息内容
 print(" [x] Sent 'Hello World!'")
 connection.close()  # 队列关闭
