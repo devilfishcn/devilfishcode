c09d6a053135beca76a9d0bd51479a5e8c5fd6bd
devilfishcn <devilfishcn@126.com>
2017-09-21 18:49:56 +0800

    修改print函数格式

--- rabbit/subscriber.py
+++ rabbit/subscriber.py
@@ -10,7 +10,7 @@ queue_name = result.method.queue
 print("random queuename:", queue_name)
 channel.queue_bind(exchange='logs',queue=queue_name)
 def callback(ch, method, properties, body):
-    print(" [x] %r" % body)
+    print "receive msg: %s" % body
     time.sleep(3)
 channel.basic_consume(callback,queue=queue_name,no_ack=True)
 channel.start_consuming()
