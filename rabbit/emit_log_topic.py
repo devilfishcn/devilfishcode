72b11df8abf2ef37e9250726fe7476702dcf834a
devilfishcn <devilfishcn@126.com>
2017-09-25 15:10:45 +0800

    增加rpc

--- rabbit/emit_log_topic.py
+++ rabbit/emit_log_topic.py
@@ -6,8 +6,7 @@ import sys
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='topic_logs',
-                         exchange_type='topic')
+channel.exchange_declare(exchange='topic_logs',exchange_type='topic')
 
 routing_key = sys.argv[1] if len(sys.argv) > 2 else 'anonymous.info'
 message = ' '.join(sys.argv[2:]) or 'Hello World!'
