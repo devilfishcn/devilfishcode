387e083bb687372c19f52bbaf08d2e6f7d458831
devilfishcn <devilfishcn@126.com>
2017-09-26 12:58:52 +0800

    增加确认功能

--- rabbit/task.py
+++ rabbit/task.py
@@ -4,17 +4,20 @@ import pika
 import sys
 import datetime
 starttime = datetime.datetime.now()
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='47.94.206.52'))
 channel = connection.channel()
 channel.queue_declare(queue='task_queue')
 #channel.queue_declare(queue='task_queue', durable=True)
 
 message = ' '.join(sys.argv[1:]) or "Hello World!"
 
-for i in range(10000):
+for i in range(20000):
     channel.basic_publish(exchange='',routing_key='task_queue',body=message)
     #channel.basic_publish(exchange='',routing_key='task_queue',body=message,properties=pika.BasicProperties(delivery_mode = 2,))
     print(" [x] Sent %r" % message)
 connection.close()
 endtime = datetime.datetime.now()
-print (endtime - starttime).seconds
+print endtime - starttime
+
+#测试结果：
+#（1）1万，没有序列化，0:00:07.937000，0:01:01.568000，0:00:04.769000，0:00:01.747000，0:00:03.641000，0:00:02.691000
--- rabbit/worker.py
+++ rabbit/worker.py
@@ -1,20 +1,26 @@
+# -*- coding: utf-8 -*-
+#
 import pika
 import time
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+import datetime
+starttime = datetime.datetime.now()
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='47.94.206.52'))
 channel = connection.channel()
 
-channel.queue_declare(queue='task_queue', durable=True)
-print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.queue_declare(queue='task_queue')
+#channel.queue_declare(queue='task_queue', durable=True)
+#print(' [*] Waiting for messages. To exit press CTRL+C')
+
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
-    time.sleep(body.count(b'.'))
-    print(" [x] Done")
+    #time.sleep(body.count(b'.'))
+    #print(" [x] Done")
     ch.basic_ack(delivery_tag = method.delivery_tag)
 
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(callback,
-                      queue='task_queue')
+#channel.basic_qos(prefetch_count=1)
+channel.basic_consume(callback,queue='task_queue')
 
 channel.start_consuming()
