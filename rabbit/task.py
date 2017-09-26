678749c2e76b8f41c57412f919ec5263b8b36906
devilfishcn <devilfishcn@126.com>
2017-09-26 14:24:06 +0800

    修改消息

--- rabbit/task.py
+++ rabbit/task.py
@@ -9,9 +9,11 @@ channel = connection.channel()
 channel.queue_declare(queue='task_queue')
 #channel.queue_declare(queue='task_queue', durable=True)
 
-message = ' '.join(sys.argv[1:]) or "Hello World!"
 
-for i in range(20000):
+
+for i in range(50000):
+    message = ' '.join(sys.argv[1:]) or "Hello World!"
+    message= message +str(i)
     channel.basic_publish(exchange='',routing_key='task_queue',body=message)
     #channel.basic_publish(exchange='',routing_key='task_queue',body=message,properties=pika.BasicProperties(delivery_mode = 2,))
     print(" [x] Sent %r" % message)
